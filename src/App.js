52d381a8858a75642cf90411b4e45fe0d65c5c1e
ShabanK <shabankhawar99@gmail.com>
2021-01-27 14:31:22 +0500

    aight done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,65 @@
 import React from "react"
+import Axios from "axios"
+
 import './App.css';
 
 class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      isLoading: true,
+      imageURL: "",
+      imageCounter: 1
+    }
+    this.fetchNewImage = () => {
+      this.setState({isLoading: true})
+      Axios.get(`https://jsonplaceholder.typicode.com/photos/${this.state.imageCounter}`)
+      .then(response=>{
+        console.log(response.data.url)
+        this.setState({imageURL: response.data.url, isLoading: false, imageCounter: this.state.imageCounter+1})
+      })
+    }
+  }
+
+  //import axios for use
+  //we get the image src/url
+  //we apply that url to our img tag
+
+  //we create a button
+  //on click function fires
+  //function calls the next image
+  //state gets updated accordingly
+
+  componentDidMount(){
+    console.log("Component did mount was fired")
+    Axios.get(`https://jsonplaceholder.typicode.com/photos/${this.state.imageCounter}`)
+    .then(response=>{
+      console.log(response.data.url)
+      this.setState({imageURL: response.data.url, isLoading: false, imageCounter: this.state.imageCounter+1})
+    })
+
+  }
+
+
+
+
+  
   render(){
-    return (<div>Hello</div>)
+    return (
+    <div className="container">
+      <div className="imagediv">
+
+
+      {this.state.isLoading?
+      <p>LOADING...</p> :
+      <img src={this.state.imageURL} alt="OH NO" width={300} height={300} ></img>
+    }
+    </div>
+    <div className="buttondiv">
+      <button onClick={this.fetchNewImage} disabled={this.state.isLoading} >Fetch New Image</button>
+    </div>
+    </div>
+    )
   }
 }
 
